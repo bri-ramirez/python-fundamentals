4a1587621a0c89132c0ed3035cf0ca3056ced6d3
bri-ramirez <briggitte.ramirez13@gmail.com>
2022-09-08 02:44:44 -0300

    blucle for baásico1

---
+++ bucle_for_basico1.py
@@ -0,0 +1,30 @@
+for num in range(0, 151):
+    print(num)
+
+for num in range(5, 1000):
+    if num % 5 == 0:
+        print(num)
+
+for num in range(1,101):
+    if num % 10 == 0:
+        print("Coding Dojo")
+    elif num % 5 == 0:
+        print("Coding")
+    else:
+        print(num)
+
+sum = 0
+for num in range(0, 500000):
+    if num % 2 != 0:
+        sum = sum + num
+print(sum)
+
+for num in range(2018, 0,-4):
+    print(num)
+
+lowNum = 2
+higtNum = 9
+mult = 3
+for num in range(lowNum, higtNum + 1):
+    if num % mult == 0:
+        print(num)
